571e625f8d6aeb1d766ca5d01fe3142e93400c8d
Florian Klampfer <mail@qwtel.com>
2024-08-05 21:41:58 +0700

    Reapply "Commit"
    This reverts commit 6c0b0bb9e4fe139cb8832767dd4c1fc73ce66309.

--- src/wasm.h
+++ src/wasm.h
@@ -26,8 +26,8 @@
 #define TJS_WASM_H
 
 // #include <m3_api_wasi.h>
-#include <wasm3.h>
+// #include <wasm3.h>
 // XXX: Needed for enumerating a module's functions
-#include <m3_env.h>
+// #include <m3_env.h>
 
 #endif
