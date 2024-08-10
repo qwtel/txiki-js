dd37f54029944ccc33e933bb52aef8a19db3f5ac
Florian Klampfer <mail@qwtel.com>
2024-08-10 08:48:33 +0700

    Commit

--- build.zig
+++ build.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-const tjs_version: std.SemanticVersion = .{ .major = 24, .minor = 6, .patch = 1, .pre = "" };
+const tjs_version: std.SemanticVersion = .{
+    .major = 24,
+    .minor = 6,
+    .patch = 2,
+    .pre = "",
+};
 
 const targets: []const std.Target.Query = &.{
     .{ .cpu_arch = .aarch64, .os_tag = .macos },
