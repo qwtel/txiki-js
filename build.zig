7cd8465032bd3b3616432d21a3e53238970411f5
Florian Klampfer <mail@qwtel.com>
2024-08-10 10:09:08 +0700

    Commit

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ const tjs_version: std.SemanticVersion = .{
     .major = 24,
     .minor = 7,
     .patch = 0,
-    .pre = "-zig.0",
+    .pre = "-zig.1",
 };
 
 const targets: []const std.Target.Query = &.{
