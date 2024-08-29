1f147877d73557a774fe20a5d10261ef49e1a079
Florian Klampfer <mail@qwtel.com>
2024-08-29 12:47:01 +0700

    build: bump version

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ const tjs_version: std.SemanticVersion = .{
     .major = 24,
     .minor = 7,
     .patch = 0,
-    .pre = "-zig.2",
+    .pre = "-zig.3",
 };
 
 const targets: []const std.Target.Query = &.{
